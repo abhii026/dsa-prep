9013f7c26593b4eb536b00ceeb05cc32a6cc295f
Abhishek Singh <iabhii026@gmail.com>
2026-03-25 10:07:17 +0530

    Renamed folders and restructured DSA repo

--- patterns/P28.java
+++ patterns/P28.java
@@ -16,7 +16,8 @@ public class P28 {
     }
     static void Pattern28(int n){
         for (int i = 0; i < 2*n; i++) {
-            int TotalCol= i > n? 2*n-i : i;
+            int     TotalCol;
+            TotalCol = i > n? 2*n-i : i;
             int no_spaces=n-TotalCol;
             for (int k = 0; k < no_spaces; k++) {
                 System.out.print(" ");
